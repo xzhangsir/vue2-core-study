2c92108ee083b55bb738a55da9c85071707fd439
张鑫 <2916733574@qq.com>
2022-09-13 11:05:27 +0800

    初始化Vue

--- dist/vue.js
+++ dist/vue.js
@@ -1,12 +1,23 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-	typeof define === 'function' && define.amd ? define(factory) :
-	(global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.Vue = factory());
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.Vue = factory());
 })(this, (function () { 'use strict';
 
-	function Vue() {}
+  function initMixin(Vue) {
+    Vue.prototype._init = function (options) {
+      console.log(options);
+    };
+  }
 
-	return Vue;
+  function Vue(options) {
+    // 初始化
+    this._init(options);
+  }
+
+  initMixin(Vue);
+
+  return Vue;
 
 }));
 //# sourceMappingURL=vue.js.map
--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-function Vue() {}
+import { initMixin } from './init'
+
+function Vue(options) {
+  // 初始化
+  this._init(options)
+}
+
+initMixin(Vue)
 
 export default Vue
